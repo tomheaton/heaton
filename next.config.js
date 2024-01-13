85464c5283668737fe4a5df9e54e3b00e8d32df3
Tom Heaton <thomas.m.heaton@gmail.com>
2024-01-13 02:19:46 +0000

    new things

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true,
-    typedRoutes: true,
-  },
 };
 
 module.exports = nextConfig;
---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  // darkMode: "class",
+  content: ["./src/components/**/*.{js,ts,jsx,tsx}", "./src/app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
